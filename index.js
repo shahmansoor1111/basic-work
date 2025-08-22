2c904086746e4e001801c274b0702ec6e2405ec2
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-22 21:20:15 +0500

    yes

---
+++ index.js
@@ -0,0 +1,19 @@
+// Task 1
+// let task = ["mansoor", "faraz", "wahab"]
+// console.log(task.length)
+
+// task 2
+let charachter = ["mansoor", "faraz", "wahab"]
+console.log(charachter.at(1))
+
+// Task 3
+// let slice = "faraz mansoor wahab"
+// console.log(slice.slice(1,1))
+ 
+// Task 4
+// let subString = "faraz mansoor wahab"
+// console.log(subString.substring(0,1))
+
+// Task 5
+// let case1 = "faraz" 
+// console.log(case1.toUpperCase())
